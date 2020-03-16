1057c582202d71a4ce540eb236c7d99a1160b6cc
root <1306144500@qq.com>
2020-03-16 17:10:24 +0800

    第一次更新

---
+++ git666/src/com/ecut/class1.java
@@ -0,0 +1,13 @@
+package com.ecut;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("git 1");
+        System.out.println("git 1");
+        System.out.println("git 1");
+        System.out.println("git 1");
+        System.out.println("git 1");
+        System.out.println("zzzz");
+        System.out.println("zzzzzzzzz");
+    }
+}
